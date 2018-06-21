ad3b16d1a73a016d8791a3a7abdb58c963f53f92
SoftElement <softelement.gap@gmail.com>
2018-06-21 23:55:52 +0200

    Added Seed entity, validations, SeedSeeder.

---
+++ PlantTycoonHelper/SeedSeeder.cs
@@ -0,0 +1,36 @@
+﻿using PlantTycoon.Data;
+using PlantTycoon.Domain;
+using System;
+using System.Linq;
+
+namespace PlantTycoonHelper
+{
+    public class SeedSeeder
+    {
+        private readonly SeedCalculator calculator;
+
+        public SeedSeeder(SeedCalculator seedCalculator)
+        {
+            this.calculator = seedCalculator;
+        }
+
+        public void Reseed()
+        {
+            calculator.SetSeed(Tuple.Create('A', 1, 1), FlowerType.Arthurium, PlantType.Ananas);
+        }
+    }
+
+    public class SeedCalculator
+    {
+        public void SetSeed(Tuple<char, int, int> position, FlowerType flower, PlantType plant)
+        {
+            using (var dbContext = new PlantTycoonContext())
+            {
+                //var seed = dbContext.Seeds.FirstOrDefault(x => x.Position == position);
+                var seed = new Seed(position, flower, plant);
+                dbContext.SaveChanges();
+            }
+        }
+
+    }
+}
